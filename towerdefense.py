1d35867d7653827f5ebe20e96a3c8614a8f563c9
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2022-04-01 11:10:32 +0100

    c

--- towerdefense.py
+++ towerdefense.py
@@ -1,1 +1,13 @@
 import pygame
+from pygame.locals import *
+running = True
+root = pygame.display.set_mode((0, 0, pygame.FULLSCREEN))
+clock = pygame.time.Clock()
+fps = 60
+while running:
+    pygame.display.update()
+    root.fill((0, 255, 0))
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
