2e73ba95e910246d6b5b2b75ca57caeebd38f899
helloworld3200 <92115307+helloworld3200@users.noreply.github.com>
2022-04-05 13:43:12 +0100

    c

--- towerdefense.py
+++ towerdefense.py
@@ -7,7 +7,10 @@ fps = 60
 while running:
     pygame.display.update()
     root.fill((0, 255, 0))
-    clock.tick(60)
+    clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.draw.rect(root, (169, 169, 169), 0, root.get_size()[1], 3000, 200)
+pygame.quit()
+pygame.display.quit()
